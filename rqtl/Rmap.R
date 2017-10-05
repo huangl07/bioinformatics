7c5a4d7d4819563e015ffd1691486920230d5894
huangl07 <290012645@qq.com>
2017-10-05 23:21:31 +0800

    all

--- rqtl/Rmap.R
+++ rqtl/Rmap.R
@@ -1,10 +1,5 @@
 library(qtl)
 mapthis<-read.cross("csv",file="mapthis.csv",estimate.map=FALSE)
-pdf("mapthis.miss.pdf")
-par(mfrow=c(1,2), las=1, cex=0.8)
-plot(ntyped(mapthis), ylab="No. typed markers", main="No. genotypes by individual",type="h")
-plot(ntyped(mapthis,"mar"), ylab="No. typed individuals", main="No. genotypes by individual",type="h")
-dev.off()
 #miss
 nt.bymar <- ntyped(mapthis, "mar")
 todrop <- names(nt.bymar[nt.bymar < nind(mapthis)*0.7])
@@ -21,12 +16,8 @@ mapthis <- subset(mapthis, ind=-wh[,2])
 #group
 mapthis <- est.rf(mapthis)
 mapthis <- formLinkageGroups(mapthis, max.rf=0.35, min.lod=6, reorgMarkers=TRUE)
-#plot.RF
-pdf("mapthis.rf.pdf")
-plotRF(mapthis)
-dev.off()
-#adjust group
-toswitch <- markernames(mapthis, chr=c(2,8))
+#adjust 
+toswitch<-markernames(mapthis,chr=c(1,9:11))
 mapthis <- switchAlleles(mapthis, toswitch)
 mapthis <- est.rf(mapthis)
 mapthis <- formLinkageGroups(mapthis, max.rf=0.35, min.lod=6, reorgMarkers=TRUE)
