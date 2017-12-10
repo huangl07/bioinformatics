4ec19ab146c75b5a61eb06d810f9fb9462d97ef7
huangl07 <290012645@qq.com>
2017-12-10 15:48:32 +0800

    drawbinmap

--- Pipeline/06.GMAP/bin/bin/drawbinmap.R
+++ Pipeline/06.GMAP/bin/bin/drawbinmap.R
@@ -19,10 +19,7 @@ Usage example:
 	
 Usage:
 	--mark	map file
-	--trt	trt file
-	--pop	pop type
 	--out	out dir
-	--num	pm number
 	--help		usage
 \n")
 	q(status=1);
@@ -31,20 +28,19 @@ times<-Sys.time()
 library('qtl');
 if ( !is.null(opt$help) ) { print_usage(spec) }
 if ( is.null(opt$mark) ) { print_usage(spec) }
-if ( is.null(opt$trt) ) { print_usage(spec) }
 if ( is.null(opt$pop) ) { print_usage(spec) }
-if ( is.null(opt$num) ) { opt$num=1000; }
 if ( is.null(opt$out) ) { opt$out="./";}
 if(!dir.exists(opt$out)){dir.create(opt$out)}
 
-d<-read.table("10.mapEva/total.csv",head=TRUE,sep=",")
+d<-read.table(opt$mark,head=TRUE,sep=",")
 d<-na.omit(d)
 cols=c("green", "blue", "red","grey40")
 colnames(d)[2:3]=c("chr","pos")
 chr=unique(d$chr);
 for(i in chr){
-	d$start[d$chr==i] = c(1:length(d$pos[d$chr==i]));
-	d$end[d$chr==i] = c(2:(length(d$pos[d$chr==i])+1));
+	pos=d[d$chr==i]
+	d$start[d$chr==i]=(d$pos[c(1,1:(length(pos)-1))]+d$pos[c(1:length(pos))])/2
+	d$end[d$chr==i]=(d$pos[c(2:(length(pos)),length(pos))]+d$pos[c(1:length(pos))])/2
 }
 chr.len <- tapply(d$end, d$chr, max)
 x.brks <- cumsum(chr.len) - chr.len/2
@@ -52,16 +48,25 @@ chr.cum.len <- c(0, cumsum(chr.len)[-length(chr.len)])
 names(chr.cum.len) <- names(chr.len)
 d$start <- d$start + chr.cum.len[d$chr]
 d$end <- d$end + chr.cum.len[d$chr]
-pdf("test.pdf");
+names(cols) <- c("A", "B","H", "-")
+
+pdf(paste(opt$out,"pdf",sep="."));
 plot(5, type = "n", xlim = c(0, max(d$end)), ylim = c(0,ncol(d) - 4), xaxt = "n")
 axis(side = 1, at = x.brks, labels = unique(d$chr))
-names(cols) <- c("A", "B","H", "-")
 for (i in 5:ncol(d)) {
     rect(xleft = d$start, ybottom = i - 5, xright = d$end,ytop = i - 4, col = cols[d[, i]], border = NA)
 }
 abline(v = c(0, cumsum(chr.len)), col = "grey80", lwd = 0.5)
 dev.off()
 
+png(paste(opt$out,"png",sep="."));
+plot(5, type = "n", xlim = c(0, max(d$end)), ylim = c(0,ncol(d) - 4), xaxt = "n")
+axis(side = 1, at = x.brks, labels = unique(d$chr))
+for (i in 5:ncol(d)) {
+    rect(xleft = d$start, ybottom = i - 5, xright = d$end,ytop = i - 4, col = cols[d[, i]], border = NA)
+}
+abline(v = c(0, cumsum(chr.len)), col = "grey80", lwd = 0.5)
+dev.off()
 
 
 escaptime=Sys.time()-times;
