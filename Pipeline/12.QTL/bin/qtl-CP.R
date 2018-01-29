a3371f2bc1a54028dfc3cc3b2056640ea3dccc44
huangl07 <290012645@qq.com>
2018-01-29 21:00:31 +0800

    qtl

--- Pipeline/12.QTL/bin/qtl-CP.R
+++ Pipeline/12.QTL/bin/qtl-CP.R
@@ -62,9 +62,10 @@ for (i in 1:length(phe.name)){
 	plotPheno(d,pheno.col=phe.name[i])
 }
 dev.off()
+chr=chrnames(d)
 for(i in 1:length(phe.name)){
 	if(phe.name[i] == "Genotype" | phe.name[i]=="sampleID"){next;}
-	print(paste(opt$method,"trait",phe.name[i],sep="\t"))
+	print(paste("trait",phe.name[i],sep="\t"))
 	scan<-scanone(d,pheno.col=phe.name[i]);
 	scan.pm<-scanone(d,pheno.col=phe.name[i],n.perm=opt$num);
 	markerid<-find.marker(d,chr=scan$chr,pos=scan$pos)
@@ -72,19 +73,22 @@ for(i in 1:length(phe.name)){
 	write.table(file=paste(phe.name[i],".scan.csv",sep=""),sep="\t",outd,row.names=FALSE)
 	write.table(file=paste(phe.name[i],".pm.csv",sep=""),sep="\t",scan.pm);
 	scan.result<-summary(scan, perms=scan.pm, pvalues=TRUE)
+	theshold=3;
 	if(min(scan.result$pval) >0.1){
 		scan.result<-summary(scan,format="tabByCol",threshold=3,drop=1)
 		if(length(rownames(scan.result$lod)) < 1){
+			theshold=2.5;
 			scan.result<-summary(scan,format="tabByCol",threshold=2.5,drop=1)
 		}
 		pm.result<-c(3,2.5)
 		legend=pm.result
 	}else{	
+		theshold=summary(scan.pm,alpha=0.01);
 		scan.result<-summary(scan,format="tabByCol",perms=scan.pm,alpha=0.1,drop=1)
 		if(length(rownames(scan.result$lod)) < 1){
+			theshold=summary(scan.pm,alpha=0.05);
 			scan.result<-summary(scan,format="tabByCol",alpha=0.05,drop=1)
 		}
-
 		pm.result<-summary(scan.pm,alpha=c(0.01,0.05))
 		legend=paste(rownames(pm.result),round(pm.result,2))
 	}
@@ -101,24 +105,52 @@ for(i in 1:length(phe.name)){
 	if(length(rownames(scan.result$lod)) < 1){
 		next;
 	}
-	qtlname=paste(phe.name[i],c(1:length(rownames(scan.result$lod))))
-	qtl<-makeqtl(d,chr=scan.result$lod$chr,pos=scan.result$lod$pos,qtl.name=qtlname)
+	qdata<-NULL
+	n=0;
+	for (j in chr){
+		subd=which(outd$chr==j & outd$lod > theshold[1])
+		if(length(subd) < 1){next;}
+		start=1000;
+		end=-1;
+		for(k in c(2:length(subd))){
+			if(subd[k]-subd[k-1] < 2){
+				if(subd[k-1] < start){start=subd[k-1]}
+				if(subd[k] > end){end=subd[k]}
+			}else{	
+				if (!is.null(qdata)){
+					qdata<-rbind(qdata,data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end]))
+				}else{
+					qdata<-data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end])
+				}
+				n=n+1
+				start=subd[k]
+				end=subd[k]
+			}
+		}
+		if(start != 1000){
+			n=n+1;
+			if (!is.null(qdata)){
+				qdata<-rbind(qdata,data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end]))
+			}else{
+				qdata<-data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end])
+			}
+		}
+	}
+	qtlname=paste(phe.name[i],c(1:length(qdata$n)))
+	qtl<-makeqtl(d,chr=qdata$chr,pos=qdata$pos,qtl.name=qtlname)
 	fitqtl<-fitqtl(cross=d,qtl=qtl,get.est=TRUE,pheno.col=i)
 	markerid<-find.marker(d,chr=qtl$chr,pos=qtl$pos)
 	var<-fitqtl$result.drop[,"%var"]
 	if (length(qtl$name) == 1){var<-fitqtl$result.full["Model","%var"]}
-	data<-data.frame(marker=markerid,chr=scan.result$lod$chr,pos=scan.result$lod$pos,lod=scan.result$lod$lod,var=var,pm1=pm.result[1],pm2=pm.result[2])
+	data<-data.frame(marker=markerid,chr=qdata$chr,pos=qdata$pos,lod=qdata$lod,var=var,pm1=pm.result[1],pm2=pm.result[2],start=outd$pos[start],end=outd$pos[end])
 	for(j in 1:length(qtlname)){
-		insert<-bayesint(scan,chr=qtl$chr[j],expandtomarkers=FALSE,prob=0.99)
-		data$start[j]=min(insert$pos);
-		data$end[j]=max(insert$pos);
 		data$mark1[j]=find.marker(d,chr=qtl$chr[j],data$start[j])
 		data$mark2[j]=find.marker(d,chr=qtl$chr[j],data$end[j])
 		pdf(paste(phe.name[i],".",qtl$name[j],".PXG.pdf",sep=""))
-		plotPXG(d,data$marker[j],pheno.col=phe.name[i])
+		plotPXG(d,data$marker[j],pheno.col=i)
 		dev.off()
 		png(paste(phe.name[i],".",qtl$name[i],".PXG.png",sep=""))
-		plotPXG(d,data$marker[j],pheno.col=phe.name[i])
+		plotPXG(d,data$marker[j],pheno.col=i)
 		dev.off()
 	}
 	write.table(file=paste(phe.name[i],".qtl.csv",sep=""),sep="\t",data,row.names=FALSE)
--- Pipeline/12.QTL/bin/qtl-NOCP.R
+++ Pipeline/12.QTL/bin/qtl-NOCP.R
@@ -78,23 +78,23 @@ for(i in 1:length(phe.name)){
 	write.table(file=paste(phe.name[i],".pm.csv",sep=""),sep="\t",scan.pm);
 	scan.result<-summary(scan, perms=scan.pm, pvalues=TRUE)
 	pm.the<-1;
-	if(min(scan.result$pval) >0.01){
+	theshold=3;
+	if(min(scan.result$pval) >0.1){
 		scan.result<-summary(scan,format="tabByCol",threshold=3,drop=1)
-		pm.the=3;
 		if(length(rownames(scan.result$lod)) < 1){
-			pm.the=2.5;
+			theshold=2.5;
 			scan.result<-summary(scan,format="tabByCol",threshold=2.5,drop=1)
 		}
 		pm.result<-c(3,2.5)
 		legend=pm.result
 	}else{	
-		pm.result<-summary(scan.pm,alpha=c(0.01,0.05))
-		scan.result<-summary(scan,format="tabByCol",perms=scan.pm,alpha=0.01,drop=1)
-		pm.the=pm.result[1]
+		theshold=summary(scan.pm,alpha=0.01);
+		scan.result<-summary(scan,format="tabByCol",perms=scan.pm,alpha=0.1,drop=1)
 		if(length(rownames(scan.result$lod)) < 1){
-			pm.the=pm.result[2]
+			theshold=summary(scan.pm,alpha=0.05);
 			scan.result<-summary(scan,format="tabByCol",alpha=0.05,drop=1)
 		}
+		pm.result<-summary(scan.pm,alpha=c(0.01,0.05))
 		legend=paste(rownames(pm.result),round(pm.result,2))
 	}
 	pdf(file=paste(phe.name[i],".scan.pdf",sep=""))
@@ -107,11 +107,39 @@ for(i in 1:length(phe.name)){
 	abline(h=pm.result,col=rainbow(length(pm.result)))
 	legend("topright",legend=legend,col=rainbow(length(pm.result)),pch=1)
 	dev.off()
-	if(length(rownames(scan.result$lod)) < 1){
-		next;
+	qdata<-NULL
+	n=0;
+	for (j in chr){
+		subd=which(outd$chr==j & outd$lod > theshold[1])
+		if(length(subd) < 1){next;}
+		start=1000;
+		end=-1;
+		for(k in c(2:length(subd))){
+			if(subd[k]-subd[k-1] < 2){
+				if(subd[k-1] < start){start=subd[k-1]}
+				if(subd[k] > end){end=subd[k]}
+			}else{	
+				if (!is.null(qdata)){
+					qdata<-rbind(qdata,data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end]))
+				}else{
+					qdata<-data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end])
+				}
+				n=n+1
+				start=subd[k]
+				end=subd[k]
+			}
+		}
+		if(start != 1000){
+			n=n+1;
+			if (!is.null(qdata)){
+				qdata<-rbind(qdata,data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end]))
+			}else{
+				qdata<-data.frame(chr=j,n=n,pos=outd$pos[start:end][which.max(outd$lod[start:end])],lod=max(outd$lod[start:end]),start=outd$pos[start],end=outd$pos[end])
+			}
+		}
 	}
-	qtlname=paste(phe.name[i],c(1:length(rownames(scan.result$lod))),sep="-")
-	qtl<-makeqtl(d,chr=scan.result$lod$chr,pos=scan.result$lod$pos,qtl.name=qtlname)
+	qtlname=paste(phe.name[i],c(1:length(qdata$n)))
+	qtl<-makeqtl(d,chr=qdata$chr,pos=qdata$pos,qtl.name=qtlname)
 	if(opt$pop =="bcsft") {	
 		fitqtl<-fitqtl(cross=d,qtl=qtl,pheno.col=i)
 	}else{
@@ -120,18 +148,15 @@ for(i in 1:length(phe.name)){
 	markerid<-find.marker(d,chr=qtl$chr,pos=qtl$pos)
 	var<-fitqtl$result.drop[,"%var"]
 	if (length(qtl$name) == 1){var<-fitqtl$result.full["Model","%var"]}
-	data<-data.frame(marker=markerid,chr=scan.result$lod$chr,pos=scan.result$lod$pos,lod=scan.result$lod$lod,var=var,threshold1=pm.result[1],threshold2=pm.result[2])
+	data<-data.frame(marker=markerid,chr=qdata$chr,pos=qdata$pos,lod=qdata$lod,var=var,pm1=pm.result[1],pm2=pm.result[2],start=outd$pos[start],end=outd$pos[end])
 	for(j in 1:length(qtlname)){
-		insert<-lodint(scan,chr=qtl$chr[j],expandtomarkers=TRUE,drop=0.000001)
-		data$start[j]=min(insert$pos);
-		data$end[j]=max(insert$pos);
 		data$mark1[j]=find.marker(d,chr=qtl$chr[j],data$start[j])
 		data$mark2[j]=find.marker(d,chr=qtl$chr[j],data$end[j])
 		pdf(paste(phe.name[i],".",qtl$name[j],".PXG.pdf",sep=""))
-		plotPXG(d,data$marker[j],pheno.col=phe.name[i])
+		plotPXG(d,data$marker[j],pheno.col=i)
 		dev.off()
 		png(paste(phe.name[i],".",qtl$name[i],".PXG.png",sep=""))
-		plotPXG(d,data$marker[j],pheno.col=phe.name[i])
+		plotPXG(d,data$marker[j],pheno.col=i)
 		dev.off()
 	}
 	write.table(file=paste(phe.name[i],".qtl.csv",sep=""),sep="\t",data,row.names=FALSE)
